3fd5fc7a842f6a7dd569722568af51bdc898f755
bohai <bohai@joyy.com>
2022-11-14 16:55:57 +0800

    vue3+andt3

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,12 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+</script>
+
 <template>
-	<img alt="Vue logo" src="./assets/logo.png">
-	<HelloWorld msg="Welcome to Your Vue.js App"/>
 	<form action="/process_post" method="post">
+    <a-date-picker />
+    <a-input v-model:value="value" placeholder="Basic usage" />
 		<p class="form-item">
 			<label for="">文件名:</label>
 			<input type="text" name="fileName" placeholder="文件名: xxx.vue">
@@ -19,26 +24,44 @@
 		</p>	
 		<input type="submit" value="提交">
 	</form>
+  <!-- <header>
+    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+    </div>
+  </header>
+
+  <main>
+    <TheWelcome />
+  </main> -->
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-	name: 'App',
-	components: {
-		HelloWorld
-	}
+<style scoped>
+header {
+  line-height: 1.5;
 }
-</script>
 
-<style>
-#app {
-	font-family: Avenir, Helvetica, Arial, sans-serif;
-	-webkit-font-smoothing: antialiased;
-	-moz-osx-font-smoothing: grayscale;
-	text-align: center;
-	color: #2c3e50;
-	margin-top: 60px;
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,19 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+/*
+ * @Author: BoHai
+ * @Date: 2022-11-14 15:22:41
+ * @LastEditTime: 2022-11-14 16:54:04
+ * @LastEditors: BoHai
+ * @Description: 
+ * @FilePath: /backstageeditapp/src/main.js
+ */
+import { createApp } from 'vue';
+import App from './App.vue';
+import { Input, DatePicker, message } from 'ant-design-vue';
+import 'ant-design-vue/dist/antd.css';
+import './assets/main.css';
 
-createApp(App).mount('#app')
+const app = createApp(App); 
+app.use(Input);
+app.use(DatePicker);
+app.config.globalProperties.$message = message;
+app.mount('#app')
